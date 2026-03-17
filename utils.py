d39f691be36b3e520f714b90c1fa44a637cea941
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 17:36:45 +0100

    JAVAS: Add utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+```python
+# utils.py
+from sqlalchemy.ext.asyncio import AsyncSession
+from .database import async_session
+
+async def get_db():
+    async with async_session() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+```
+
+###
